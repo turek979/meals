ab1c3337b6c5190a0e795df9e105fb68e71757d9
J.Turowski <turek.priv@gmail.com>
2024-04-23 11:08:03 +0200

    add implicit animation on favorite icon

--- lib/screens/meal_screen.dart
+++ lib/screens/meal_screen.dart
@@ -32,7 +32,19 @@ class MealScreen extends ConsumerWidget {
                   content: Text(
                       wasAdded ? 'Meal added as a favorite' : 'Meal removed')));
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                    turns:
+                        Tween<double>(begin: 0.8, end: 1.0).animate(animation),
+                    child: child);
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           )
         ],
       ),
